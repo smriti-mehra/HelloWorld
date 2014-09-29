38c059be2de7da91311fe1166c7119a162a78c13
Smriti <smriti.mehra@xaxis.com>
2014-09-29 15:50:38 -0400

    my Hello world project

---
+++ src/com/test/HelloWorld.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package com.test;
+
+/**
+ * @author Smriti.Mehra
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * 
+	 */
+	public HelloWorld() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World testing");
+
+	}
+
+}
